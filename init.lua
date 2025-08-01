e2947712373e0a08fc97d686ac622007ea961ada
johan-l-r <188136149+johan-l-r@users.noreply.github.com>
2025-07-31 21:47:41 -0500

    added colorscheme with the built-in plugin manager (vim.pack)

--- init.lua
+++ init.lua
@@ -7,9 +7,26 @@ local km = vim.keymap
 o.number = true
 o.relativenumber = true -- show relative number
 o.swapfile = false -- no swap files 
-o.shiftwidth = 2
+o.shiftwidth = 2 -- indentation is equal to 2 spaces 
 o.tabstop = 2
+o.signcolumn = "yes" -- I like the little gap in the number column 
+o.colorcolumn = "100" -- show a mark for line length
 
--- keymaps 
-km.set("n", "<leader>so", "<cmd>so<cr>", { desc = "source config" })
-km.set("n", "<leader>oe", "<cmd>:Explore<cr>", { desc = "source config" })
+-- neovim keymaps 
+km.set("n", "<leader>so", function() 
+	vim.cmd("write") 
+	vim.cmd("so %")
+
+	vim.api.nvim_echo( {{"config sourced! \n" }}, false, {}) -- prints when config is sourced
+end, { desc = "source config" })
+
+km.set("n", "<leader>oe", ":Explore<cr>", { desc = "open netrw" })
+
+-- plugins! (with vim.pack)
+vim.pack.add({
+	{ src = "https://github.com/thesimonho/kanagawa-paper.nvim" }
+})
+
+vim.cmd("colorscheme kanagawa-paper")
+
+
