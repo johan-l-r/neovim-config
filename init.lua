4d9dd70e789fbce3153ab233ea9f6ee918253547
johan-l-r <188136149+johan-l-r@users.noreply.github.com>
2025-08-03 12:38:43 -0500

    added some new keymaps

--- init.lua
+++ init.lua
@@ -9,8 +9,12 @@ local o = vim.opt
 local km = vim.keymap
 
 -- OPTIONS 
-o.number				 = true
-o.relativenumber = true  -- show relative number
+o.number 				 = true
+o.relativenumber = true
+o.ignorecase 		 = true
+o.smartcase 	 	 = true
+o.splitright 		 = true
+o.splitbelow 		 = true
 o.swapfile 			 = false -- no swap files 
 o.shiftwidth		 = 2 		 -- indentation is equal to 2 spaces 
 o.tabstop				 = 2
@@ -18,15 +22,20 @@ o.signcolumn		 = "yes" -- I like the little gap in the number column
 o.colorcolumn 	 = "100" -- show a mark for line length
 
 -- KEYMAPS 
-km.set("n", "<leader>so", function() 
-	vim.cmd("write") 
-	vim.cmd("so %")
+km.set("n", "<leader>bn", ":bnext<cr>", { desc = "goto next buffer" })
+km.set("n", "<leader>bp", ":nprevious<cr>", { desc = "goto prev buffer" })
 
-	vim.api.nvim_echo( {{"config sourced! \n" }}, false, {}) -- prints when config is sourced
-end, { desc = "source config" })
+km.set("n", "<leader>W", ":wa<cr>", { desc = "write all" })
+km.set("n", "<leader>Q", ":qa<cr>", { desc = "quit all" })
+km.set("n", "<leader>w", ":w<cr>", { desc = "write" })
+km.set("n", "<leader>q", ":q<cr>", { desc = "quit" })
+km.set("n", "<leader>R", ":restart<cr>", { desc = "restart" })
 
-
-km.set("n", "<leader>oe", ":Explore<cr>", { desc = "open netrw" })
-
-km.set("n", "<leader>bn", ":bnext<cr>", { desc = "open netrw" })
-km.set("n", "<leader>bp", ":nprevious<cr>", { desc = "open netrw" })
+km.set({ "n", "x" }, "G", "Gzz", { desc = "goto bottom of file and center the cursor" })
+km.set({ "n", "x" }, "j", "jzz", { desc = "move down and center the cursor" })
+km.set({ "n", "x" }, "k", "kzz", { desc = "move up and center the cursor" })
+km.set("n", "<C-u>", "<C-u>zz", { desc = "move half the file up and center the cursor" })
+km.set("n", "<C-d>", "<C-d>zz", { desc = "move half the file down and center the cursor" })
+km.set("n", "<leader>rc", ":e ~/.config/nvim/init.lua<cr>", { desc = "goto config" })
+km.set("n", "<leader>vs", ":vsplit<cr>", { desc = "open new vertical split" })
+km.set("n", "<leader>hs", ":split<cr>", { desc = "open new horizontal split" })
